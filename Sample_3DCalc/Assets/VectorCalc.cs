d20aad6950feb57c863f77d33b333c27a35a8232
maruton <marutongit@gmail.com>
2018-05-04 05:21:47 +0900

    1st commit.

---
+++ Sample_3DCalc/Assets/VectorCalc.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorCalc : MonoBehaviour {
+
+
+	public GameObject TargetShip;
+
+
+	void testFunc() {
+		// Calc distance
+		float dist = Vector3.Distance(TargetShip.transform.position, transform.position);
+
+		// Calc Sight Range degree
+		Vector3 targetDir = TargetShip.transform.position - transform.position;
+		float angle = Vector3.Angle(targetDir, transform.forward);
+
+		// Front=Z+  
+		Debug.Log("dist=" + dist + "  angle=" + angle);
+	}
+
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		testFunc();
+	}
+}
